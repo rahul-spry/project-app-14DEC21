32f2818323922f9c1946a717086cae14fc3b4fc2
Rahul Chavan <r.chavan619@gmail.com>
2021-12-14 20:48:02 +0530

    Delete src/app directory

--- src/app/recipes/recipe.ts
+++
@@ -1,4 +0,0 @@
-export class Recipe {
-  constructor(public name, public description, public imagePath, public price, public offerdate, 
-    public offerExpiryDate, public details, public limit) {}
-}
